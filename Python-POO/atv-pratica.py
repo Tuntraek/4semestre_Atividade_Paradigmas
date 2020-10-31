b16d9184f61d35f94dce8515a51774ec786c868b
Evair Marinho <evairmarinho@hotmail.com>
2020-10-31 16:15:14 -0300

    ajustes de validação de dados

--- Python-POO/atv-pratica.py
+++ Python-POO/atv-pratica.py
@@ -1,106 +1,124 @@
 import os
 import time
-    
+
+
 class Validador:
-    def validadorInt(self,n):
+    def validadorInt(self, n):
         try:
             n = int(input())
             return n
         except:
             print('Entrada inválida!')
-            time.sleep(4)
-            os.system("cls")
-            main()
-            Validador.validadorInt(n)
+            print('Digite novamente')
+            return False
+
 
 class Jogador:
-    def __init__ (self,nome):
+    def __init__(self, nome):
         self.__nome = nome
-        self.__pontuacao=[int,int,int,int,int]
+        self.__pontuacao = [int, int, int, int, int]
+
     def getNome(self):
         return self.__nome
+
     def getPontuacao(self):
         return self.__pontuacao
-    def setPontuacao(self,pontuacao):
-        self.__pontuacao=pontuacao
+
+    def setPontuacao(self, pontuacao):
+        self.__pontuacao = pontuacao
+
 
 class Jogo:
-    def __init__(self,jog1,jog2):
-        self.__jog1=jog1
-        self.__jog2=jog2
+    def __init__(self, jog1, jog2):
+        self.__jog1 = jog1
+        self.__jog2 = jog2
+
 
 class Ginastica(Jogo):
-    def __init__(self,jog1,jog2):
-        super().__init__(jog1,jog2)
+    def __init__(self, jog1, jog2):
+        super().__init__(jog1, jog2)
 
-    def nota(self,jog):
+    def nota(self, jog):
+        validador = Validador()
         cont = 0
         pontuacao = jog.getPontuacao()
         print('---------------------------')
         print('Jogador '+jog.getNome())
-        while(cont<5):
+        while(cont < 5):
             print('Digite a nota '+str(cont+1))
-            pontuacao[cont] = input()
+            obj = pontuacao[cont]
+            pontuacao[cont] = validador.validadorInt(obj)
 
-            jog.setPontuacao(pontuacao)   
+            jog.setPontuacao(pontuacao)
             cont = cont + 1
         print('---------------------------')
 
-    def iniciarJogo(self,jog1,jog2):
-        jogo = Ginastica(jog1,jog2)
+    def iniciarJogo(self, jog1, jog2):
+        jogo = Ginastica(jog1, jog2)
         jogo.nota(jog1)
         jogo.nota(jog2)
-        jogo.ganhador(jogo,jog1,jog2)
-                
-    def calcNota(self,jog):
+        jogo.ganhador(jogo, jog1, jog2)
+
+    def calcNota(self, jog):
         pontuacao = jog.getPontuacao()
         baixa = pontuacao[0]
         for i in pontuacao:
-            if int(i)<int(baixa):
+            if int(i) < int(baixa):
                 baixa = int(i)
-        total=int(0)
+        total = int(0)
         for i in pontuacao:
             if not(int(i) == int(baixa)):
-                total+=int(i)
+                total += int(i)
         return int(total)
 
-    def ganhador(self,jogo,jog1,jog2):
+    def ganhador(self, jogo, jog1, jog2):
         print('Nota do Jogador 1 '+jog1.getNome()+': '+str(jogo.calcNota(jog1)))
         print('Nota do Jogador 2 '+jog2.getNome()+': '+str(jogo.calcNota(jog2)))
         print('---------------------------')
-        if int(jogo.calcNota(jog1))>int(jogo.calcNota(jog2)):
+        if int(jogo.calcNota(jog1)) > int(jogo.calcNota(jog2)):
             print('Parabéns, '+jog1.getNome()+', você venceu!')
-        elif int(jogo.calcNota(jog1))==int(jogo.calcNota(jog2)):
+        elif int(jogo.calcNota(jog1)) == int(jogo.calcNota(jog2)):
             print("Empate entre "+jog1.getNome()+' e '+jog1.getNome())
         else:
             print('Parabéns, '+jog2.getNome()+', você venceu!')
-        
+
         print('---------------------------')
+
+
 def main():
-    menu() 
-    opcao = 0
+    menu()
+    opcao = False
     validador = Validador()
-    opcao = validador.validadorInt(opcao)
+    while (opcao == False):
+        opcao = validador.validadorInt(opcao)
+
     if opcao == 1:
         print('Digite o nome do jogador 1')
-        j1=input()
+        j1 = input()
         print('Digite o nome do jogador 2')
-        j2=input()
-        jog1Gin=Jogador(j1)
-        jog2Gin=Jogador(j2)
-        ginastica=Ginastica(jog1Gin,jog2Gin)
-        ginastica.iniciarJogo(jog1Gin,jog2Gin)
+        j2 = input()
+        jog1Gin = Jogador(j1)
+        jog2Gin = Jogador(j2)
+        ginastica = Ginastica(jog1Gin, jog2Gin)
+        ginastica.iniciarJogo(jog1Gin, jog2Gin)
         main()
-    elif opcao == 2 :
+    elif opcao == 2:
         main()
     elif opcao == 3:
         breakpoint
-    
-    
+    else:
+        print('Opção inexistente no menu')
+        time.sleep(2.5)
+        os.system("cls")
+        main()
+
+
 def menu():
     print("----COMPETIÇÕES ESPORTIVAS----\n")
     print("Digite uma opção\n")
     print("1 - Ginástica artística")
     print("2 - Arremesso de peso")
     print("3 - Sair do jogo")
+
+
 main()
